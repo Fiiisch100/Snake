c1273bc1d7942332f83eca69cc7fee1f12ec33f5
quirin <quirin.guenther@web.de>
2024-02-06 11:14:24 +0100

    Added Starting Menu (not connected to game)

---
+++ app/src/main/java/com/example/snake/Menu.java
@@ -0,0 +1,14 @@
+package com.example.snake;
+
+import android.view.View;
+
+public class Menu {
+    public void Highscores(View view) {
+    }
+
+    public void Play(View view) {
+    }
+
+    public void Quit(View view) {
+    }
+}
